e370dd348a96358fabef7d3e3319e1f8ce169bac
MuneerAhamed01 <muneerahamed655@gmail.com>
2023-07-30 19:37:45 +0530

    fix: pawn movement is fixed

--- os/lib/nodes/chess_movement_node.dart
+++ os/lib/nodes/chess_movement_node.dart
@@ -1,9 +1,20 @@
+import 'package:chess_os/model/chess_matrix.dart';
 import 'package:chess_os/nodes/chess_piece_node.dart';
 import 'package:flutter/foundation.dart';
 
 class ChessController extends ChangeNotifier {
-  ChessController._();
+  ChessController._() {
+    _filledPositions = [
+      for (int i = 0; i < 8; i++) ...[
+        Matrix(i, 0),
+        Matrix(i, 1),
+        Matrix(i, 7),
+        Matrix(i, 6)
+      ],
+    ];
+  }
   static final ChessController instance = ChessController._();
+  late final List<Matrix> _filledPositions;
 
   ChessPieceNode? _pickedPosition;
   ChessPieceNode? _droppedPosition;
@@ -12,25 +23,52 @@ class ChessController extends ChangeNotifier {
   ChessPieceNode? get droppedValue => _droppedPosition;
 
   set pickValue(ChessPieceNode piece) {
+    print("Picked Piece ${piece.matrix}");
     _pickedPosition = piece;
   }
 
   set dropValue(ChessPieceNode piece) {
     if (piece.matrix == _pickedPosition?.matrix) {
+      print("Clearing Because it Touch the current Position ${piece.matrix}");
       _clear();
       return;
     }
-    bool isPossibleToMove =
-        _pickedPosition!.possibleMovements().contains(piece.matrix);
+    bool isPossibleToMove = _pickedPosition!
+        .possibleMovements(_filledPositions)
+        .contains(piece.matrix);
+
     if (isPossibleToMove) {
-      _droppedPosition = piece;
-      notifyListeners();
-      _clear();
+      if (piece is! EmptyNode) {
+        bool shouldCut = _pickedPosition!.isWhite != piece.isWhite;
+        if (shouldCut) _drop(piece);
+      } else {
+        _drop(piece);
+      }
     }
+  }
+
+  _drop(ChessPieceNode piece) {
+    print("Dropped Piece ${piece.matrix}");
+    _droppedPosition = piece;
+    notifyListeners();
+    _updateTheFilledPosition(
+      toRemove: _pickedPosition?.matrix,
+      toAdd: _droppedPosition?.matrix,
+    );
+    _clear();
   }
 
   _clear() {
     _pickedPosition = null;
     _droppedPosition = null;
+  }
+
+  _updateTheFilledPosition({Matrix? toRemove, Matrix? toAdd}) {
+    if (toRemove != null) {
+      _filledPositions.remove(toRemove);
+    }
+    if (toAdd != null && !_filledPositions.contains(toAdd)) {
+      _filledPositions.add(toAdd);
+    }
   }
 }
--- os/lib/nodes/chess_piece_node.dart
+++ os/lib/nodes/chess_piece_node.dart
@@ -7,11 +7,16 @@ abstract class ChessPieceNode {
   late Matrix matrix;
   final bool isWhite;
 
+  int get _row => matrix.row;
+  int get _column => matrix.column;
+
   ChessPieceNode(this.isWhite, this.matrix);
 
   ChessPieceNode copyWith(Matrix matrix);
 
-  List<Matrix> possibleMovements();
+  List<Matrix> possibleMovements(List<Matrix> currentFilled);
+
+  bool isBlocker(Matrix position);
 }
 
 class PawnNode extends ChessPieceNode {
@@ -20,13 +25,51 @@ class PawnNode extends ChessPieceNode {
   }
 
   @override
-  PawnNode copyWith(Matrix matrix) {
-    return PawnNode(isWhite, matrix);
+  PawnNode copyWith(Matrix matrix) => PawnNode(isWhite, matrix);
+
+  @override
+  List<Matrix> possibleMovements(List<Matrix> currentFilled) {
+    List<Matrix> matrixes = [];
+    List<Matrix> crossCut = [];
+
+    if (isWhite) {
+      crossCut = currentFilled
+          .where((element) =>
+              element == Matrix(_row + 1, _column + 1) ||
+              element == Matrix(_row - 1, _column + 1))
+          .toList();
+      if (currentFilled
+          .any((element) => element == Matrix(matrix.row, matrix.column + 1))) {
+        return [...crossCut];
+      }
+      if (matrix.column == 1) {
+        matrixes.add(Matrix(matrix.row, matrix.column + 2));
+      }
+      matrixes.add(Matrix(matrix.row, matrix.column + 1));
+    } else {
+      crossCut = currentFilled
+          .where((element) =>
+              element == Matrix(_row + 1, _column - 1) ||
+              element == Matrix(_row - 1, _column - 1))
+          .toList();
+      if (matrix.column == 6) {
+        if (currentFilled.any(
+            (element) => element == Matrix(matrix.row, matrix.column - 1))) {
+          return [...crossCut];
+        }
+        matrixes.add(Matrix(matrix.row, matrix.column - 2));
+      }
+      matrixes.add(Matrix(matrix.row, matrix.column - 1));
+    }
+
+    return matrixes
+      ..removeWhere((element) => currentFilled.contains(element))
+      ..addAll(crossCut);
   }
 
   @override
-  List<Matrix> possibleMovements() {
-    return [const Matrix(0, 4)];
+  bool isBlocker(Matrix position) {
+    return position is! EmptyNode;
   }
 }
 
@@ -38,7 +81,12 @@ class EmptyNode extends ChessPieceNode {
   }
 
   @override
-  List<Matrix> possibleMovements() {
+  List<Matrix> possibleMovements(List<Matrix> currentFilled) {
     return [];
+  }
+
+  @override
+  bool isBlocker(Matrix position) {
+    return false;
   }
 }
